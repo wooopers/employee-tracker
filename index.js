22d615ff045b1ab898b56d97afd1675b1ae0d300
Kenyon Gordon <kenyongordon@MacBook-Pro-2.local>
2023-06-19 17:09:04 -0500

    update to index.js and server.js files

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const inquirer = require('inquirer');
 
-inquirer
-  .prompt([
-    // Prompt options here
-  ])
-  .then((response) => {
-    // Check user's choice and invoke corresponding functions
-    switch (response.choice) {
-      case 'View Departments':
-        viewDepartments();
-        break;
-      case 'View Roles':
-        viewRoles();
-        break;
-      case 'Add Employee':
-        addEmployee();
-        break;
-      // Add more cases for other choices
-      default:
-        console.log('Invalid choice');
-    }
-  });
+// Prompt the menu options
+function promptMenu() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'choice',
+        message: 'Choose an action:',
+        choices: [
+          'View Departments',
+          'View Roles',
+          'Add Employee',
+          'Exit'
+        ]
+      }
+    ])
+    .then((response) => {
+      // Check user's choice and invoke corresponding functions
+      switch (response.choice) {
+        case 'View Departments':
+          viewDepartments();
+          break;
+        case 'View Roles':
+          viewRoles();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Exit':
+          exit();
+          break;
+        default:
+          console.log('Invalid choice');
+      }
+    });
+}
 
 // Function to view departments
 function viewDepartments() {
@@ -37,11 +52,11 @@ function addEmployee() {
   // Implementation here
 }
 
-  
-  // Exit the application
-  function exit() {
-    console.log('Exiting...');
-    process.exit(0);
-  }
-  
-  promptMenu(); // Start the application by prompting the menu
+// Exit the application
+function exit() {
+  console.log('Exiting...');
+  process.exit(0);
+}
+
+// Start the application by prompting the menu
+promptMenu();
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const fs = require('fs');
-const { connection, performDatabaseOperations } = require('./node');
-const routes = require('./index');
+const { viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole } = require('./index');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,14 +35,102 @@ pool.getConnection((err, conn) => {
     }
     console.log('Tables created successfully.');
 
-    // Call functions to handle actions
-    performDatabaseOperations(conn);
+    // Routes
+
+    // View departments
+    app.get('/departments', (req, res) => {
+      viewDepartments(conn, (err, results) => {
+        if (err) {
+          console.error('Error viewing departments: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.json(results);
+      });
+    });
+
+    // View roles
+    app.get('/roles', (req, res) => {
+      viewRoles(conn, (err, results) => {
+        if (err) {
+          console.error('Error viewing roles: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.json(results);
+      });
+    });
+
+    // View employees
+    app.get('/employees', (req, res) => {
+      viewEmployees(conn, (err, results) => {
+        if (err) {
+          console.error('Error viewing employees: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.json(results);
+      });
+    });
+
+    // Add department
+    app.post('/departments', (req, res) => {
+      const name = req.body.name;
+      addDepartment(conn, name, (err) => {
+        if (err) {
+          console.error('Error adding department: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.send('Department added successfully.');
+      });
+    });
+
+    // Add role
+    app.post('/roles', (req, res) => {
+      const { title, salary, departmentId } = req.body;
+      addRole(conn, title, salary, departmentId, (err) => {
+        if (err) {
+          console.error('Error adding role: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.send('Role added successfully.');
+      });
+    });
+
+    // Add employee
+    app.post('/employees', (req, res) => {
+      const { firstName, lastName, roleId, managerId } = req.body;
+      addEmployee(conn, firstName, lastName, roleId, managerId, (err) => {
+        if (err) {
+          console.error('Error adding employee: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.send('Employee added successfully.');
+      });
+    });
+
+    // Update employee role
+    app.put('/employees/:id/role', (req, res) => {
+      const employeeId = req.params.id;
+      const roleId = req.body.roleId;
+      updateEmployeeRole(conn, employeeId, roleId, (err) => {
+        if (err) {
+          console.error('Error updating employee role: ' + err.stack);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+        res.send('Employee role updated successfully.');
+      });
+    });
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
 
     conn.release();
   });
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
